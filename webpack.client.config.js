e387393f258494a229b6957d802389ee3bbdd064
bmj0053 <bmj05378@gmail.com>
2024-10-13 20:37:43 +0900

    build: 재배포

--- webpack.client.config.js
+++ webpack.client.config.js
@@ -26,7 +26,7 @@ module.exports = {
       {
         test: /\.css$/,
         use: ['style-loader', 'css-loader'],
-      },
+      }, 
       {
         test: /\.(png|jpe?g|gif|svg)$/i,
         type: 'asset/resource',

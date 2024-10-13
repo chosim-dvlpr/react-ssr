37c9907b656716cbae1a57a9e8d6e9b8197fecff
bmj0053 <bmj05378@gmail.com>
2024-10-13 17:22:41 +0900

    build: 재배포 (hydrateRoot 수정 - 배열 인덱스 가리키도록)

--- src/client/main.js
+++ src/client/main.js
@@ -6,6 +6,6 @@ import App from './App.jsx';
 const initialData = window.__INITIAL_DATA__ || [];
 
 hydrateRoot(
-  document.getElementsByClassName('thumbnail-list'),
+  document.getElementsByClassName('thumbnail-list')[0],
   <App movieList={initialData.length > 0 ? initialData : []} />
 );
